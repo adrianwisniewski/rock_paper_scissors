a643bc2b0ffbaf569ebc6a8758ddfebbe0fa3601
adrianwisniewski <wisniewski.adrian@live.com>
2020-08-29 12:14:40 +0200

    add some things to scoreboard

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore =0;
+let userScore = 0;
+let computerScore =0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".scored-board")
@@ -9,6 +9,62 @@ const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
  
 
+function getComputerChoice(){
+    const choices = ['r', 'p', 's']
+    const randomNumer = Math.floor(Math.random() * 3);
+    return choices[randomNumer];
+}
+
+function win(userChoice,computerChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    console.log(userChoice);
+    console.log(computerChoice);
+}
+
+function lose(){
+    console.log("LOSE");
+}
+
+function draw(){
+    console.log("DRAW");
+}
+
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch(userChoice + computerChoice){
+        case("rs"):
+        case("sp"):
+        case("pr"):
+        win(userChoice,computerChoice);
+        break;
+        case("rp"):
+        case("sr"):
+        case("ps"):
+        lose(userChoice,computerChoice);
+        break;
+        case("rr"):
+        case("ss"):
+        case("pp"):
+        draw(userChoice,computerChoice);
+        break;
+    }
+
+}
+
+
 rock_div.addEventListener('click', function(){
-    console.log("hey you clinked on rock");
+    game("r");
 })
+paper_div.addEventListener('click', function(){
+    game("p");
+})
+
+scissors_div.addEventListener('click', function(){
+    game("s");
+})
+
+
+main();
